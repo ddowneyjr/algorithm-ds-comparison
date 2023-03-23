cd399408d45088e8c842a9f082cb83ff3b047c2a
Derrell Downey Jr <derrell.downey@gmail.com>
2023-03-23 18:57:29 -0400

    added linked list c#

---
+++ linked-list/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LinkedList {
+        public class Node <T> {
+              
+              // Data
+                public T data { get; set; }  
+
+              // Link
+                public Node<T> next {get; internal set;}
+
+              // Constructor
+                public Node (T data) {
+                        this.data = data;
+                }
+        }
+
+        public class LinkedList {
+                // properties
+                public Node<T> First {get; private set;}
+
+                public Node<T> Last {get; private set;}
+
+                public int Count {get; private set;}
+
+                // constructor. Not really needed
+                public LinkedList() {
+                        this.First = null;
+                        this.Last = null;
+                }
+        }
+}
